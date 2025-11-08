530a6eb7f9d278d01a97c8b86d8d3c09d20c52fa
Mackey CHARLES <cmackey016@yahoo.fr>
2025-11-07 22:20:48 -0500

    Creating Menu Methods

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -8,8 +8,6 @@ public class Game {
     private Player player2;
 
 
-
-
     public Game(Player player1, Player player2) {
         //TO DO Verification unicite des noms du jeu et des types par equipe
         this.player1 = player1;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,47 @@
 package org.example;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private static Scanner input = new Scanner(System.in);
     static void main() {
 
-        IO.println("Welcome to Battle Arena!");
+    menuPrincipal();
+
+
+    }
+
+    private static void menuPrincipal(){
+
+        int choice=0;
+        System.out.println("Welcome to Battle Arena!");
+        while (choice!=2)
+        {
+            System.out.println("What would you like to do ? \n \t 1. Play a game \n \t 2. Quit");
+            choice = input.nextInt();
+            if (choice==1)
+                createGame();
+        }
+
+    }
+
+    private static void createGame()
+    {
+        String player1="";
+        String player2="";
+
+        while(player1.equals(player2) || player1.isEmpty() || player2.isEmpty())
+        {
+            System.out.print("Player 1 name : ");
+            player1=input.next();
+            System.out.print("Player 2 name (different than Player 1) : ");
+            player2=input.next();
+        }
+
+        new Game(player1,player2);
+
 
     }
 }
